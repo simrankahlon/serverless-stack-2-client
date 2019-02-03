de4e58d13560da7240c186cc2beb61465367785f
simrankahlon <simran.kahlon@atos.net>
2019-02-03 12:43:44 +0530

    Adding stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "pk_test_kUjyEclhaRhOXFLEPh7ql9im",
   s3: {
     REGION: "ap-south-1",
     BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
@@ -13,22 +14,24 @@ const dev = {
     APP_CLIENT_ID: "6qqr1qg455sc2tumbiaq8mjqfl",
     IDENTITY_POOL_ID: "ap-south-1:557ca8c1-a96b-4896-9dc7-d961c63ba150"
   }
+
 };
 
 const prod = {
+  STRIPE_KEY: "pk_test_kUjyEclhaRhOXFLEPh7ql9im",
   s3: {
-    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    REGION: "ap-south-1",
     BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "YOUR_PROD_API_GATEWAY_REGION",
-    URL: "YOUR_PROD_API_GATEWAY_URL"
+    REGION: "ap-south-1",
+    URL: " https://8e5nm6sdb5.execute-api.ap-south-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_PROD_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_6GWrTcr2j",
+    APP_CLIENT_ID: "5hk4kmhl98jpdrq62s1euomjoj",
+    IDENTITY_POOL_ID: "ap-south-1:c2020135-ae4c-4539-a88c-4c6624a2e633"
   }
 };
 
